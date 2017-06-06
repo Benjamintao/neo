3c5d39207bc26ac361a228807741ce1341f02b31
Vyacheslav Efremenko <brainsapiens@gmail.com>
2017-06-06 14:27:29 +0300

    In process

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,4 @@
+// Import
 import del from 'del';
 import gulp from 'gulp';
 import pug from 'gulp-pug';
@@ -5,104 +6,106 @@ import sass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import babel from 'gulp-babel';
+import modernizr from 'gulp-modernizr';
 import concat from 'gulp-concat';
 import imagemin from 'gulp-imagemin';
 import connect from 'gulp-connect';
 import changed from 'gulp-changed';
 
-const clean = () => del('dist');
+// Paths
+const config = require('./gulpfile.config.json');
+
+// Clean
+const clean = () => del(config.paths.dist.root);
 export { clean };
 
+// Views
 export function views() {
-    return gulp.src('src/app/views/*.pug')
-        .pipe(changed('dist'))
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('dist'))
+    return gulp.src(config.paths.src.views)
+        .pipe(changed(config.paths.dist.root))
+        .pipe(pug(config.options.pug))
+        .pipe(gulp.dest(config.paths.dist.root))
         .pipe(connect.reload());
 }
 
+// Styles
+// outputStyle: :nested, :expanded, :compact, :compressed
 export function styles() {
     return gulp.src('src/app/assets/stylesheets/application.scss')
-        .pipe(changed('dist/css'))
-        .pipe(sass({
-            outputStyle: 'expanded' // :nested, :expanded, :compact, :compressed
-        }))
-        .pipe(postcss([ autoprefixer() ]))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(changed(config.paths.dist.css))
+        .pipe(sass(config.options.sass))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest(config.paths.dist.css))
         .pipe(connect.reload());
 }
 
+// Scripts
 export function scripts() {
-    return gulp.src([
-            'src/app/assets/javascripts/config.js',
-            'src/app/assets/javascripts/functions.js',
-            'src/app/assets/javascripts/variables.js',
-            'src/app/assets/javascripts/components/icons.js',
-            '!src/app/assets/javascripts/vendor/**/*.js'
-        ])
-        .pipe(changed('dist/js'))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
+    return gulp.src('src/app/assets/javascripts/application.babel.js')
+        .pipe(changed(config.paths.dist.js))
+        .pipe(babel(config.options.babel))
         .pipe(concat('application.js'))
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest(config.paths.dist.js))
         .pipe(connect.reload());
 }
 
+// Modernizr
+// Tests: https://github.com/Modernizr/Modernizr/tree/master/feature-detects
+export function modernizrBuild() {
+    return gulp.src('src/app/assets/javascripts/modernizr.js')
+        .pipe(modernizr(config.options.modernizr))
+        .pipe(gulp.dest(config.paths.dist.js));
+}
+
+// Fonts
+// Extensions: http://caniuse.com/#search=woff, http://caniuse.com/#search=woff2
 export function fonts() {
-    return gulp.src('src/app/assets/fonts/**/*.{woff,woff2}') // http://caniuse.com/#search=woff, http://caniuse.com/#search=woff2
-        .pipe(changed('dist/img'))
-        .pipe(gulp.dest('dist/fonts'))
+    return gulp.src(config.paths.src.fonts)
+        .pipe(changed(config.paths.dist.fonts))
+        .pipe(gulp.dest(config.paths.dist.fonts))
         .pipe(connect.reload());
 }
 
+// Images
 export function images() {
-    return gulp.src('src/app/assets/images/**/*.{gif,jpg,jpeg,png,svg}')
-        .pipe(changed('dist/img'))
+    return gulp.src(config.paths.src.images)
+        .pipe(changed(config.paths.dist.img))
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
+        .pipe(gulp.dest(config.paths.dist.img))
         .pipe(connect.reload());
 }
 
-export function vendors() {
-    return gulp.src([
-        'src/vendor/assets/normalize-css/normalize.css',
-        'src/vendor/assets/jquery/dist/jquery.min.js',
-        'src/vendor/assets/svg4everybody/dist/svg4everybody.min.js',
-        'src/app/assets/javascripts/vendor/modernizr-custom.js'
-    ])
-        .pipe(gulp.dest('dist/vendor'));
-}
-
+// Files
 export function files() {
-    return gulp.src('src/public/**/*')
-        .pipe(changed('dist'))
-        .pipe(gulp.dest('dist'))
+    return gulp.src(config.paths.src.files)
+        .pipe(changed(config.paths.dist.root))
+        .pipe(gulp.dest(config.paths.dist.root))
         .pipe(connect.reload());
 }
 
-export function server() {
+// Server
+export function connectServer() {
     connect.server({
-        root: 'dist/',
+        port: 1111,
+        root: config.paths.dist.root,
         livereload: true
     });
 }
 
+// Watch
 export function watch() {
-    gulp.watch('src/app/views/**/*.pug', views);
-    gulp.watch('src/app/assets/stylesheets/**/*.scss', styles);
-    gulp.watch([
-        'src/app/assets/javascripts/**/*.js',
-        '!src/app/assets/javascripts/vendor/**/*.js'
-    ], scripts);
-    gulp.watch('src/app/assets/fonts/**/*.{woff,woff2}', fonts);
-    gulp.watch('src/app/assets/images/**/*.{gif,jpg,jpeg,png,svg}', images);
-    gulp.watch('src/public/**/*', files);
+    gulp.watch(config.paths.src.views, views);
+    gulp.watch(config.paths.src.styles, styles);
+    gulp.watch(config.paths.src.scripts, scripts);
+    gulp.watch(config.paths.src.fonts, fonts);
+    gulp.watch(config.paths.src.images, images);
+    gulp.watch(config.paths.src.files, files);
 }
 
-const build = gulp.series(clean, gulp.parallel(views, styles, scripts, fonts, images, vendors, files, server, watch));
+// Build
+const build = gulp.series(clean, gulp.parallel(views, styles, scripts, modernizrBuild, fonts, images, files, connectServer, watch));
 
 export { build }
 
---
+++ src/app/assets/javascripts/application.babel.js
@@ -0,0 +1,6 @@
+// import '../../../vendor/assets/jquery/dist/jquery';
+// import '../../../vendor/assets/svg4everybody/dist/svg4everybody';
+
+// import 'src/app/assets/javascripts/_config.babel';
+// import 'src/app/assets/javascripts/_variables.babel';
+// import 'src/app/assets/javascripts/_functions.babel';
